b66db2cab737922d055d359fb402768b352ac469
CodeBrew28 <arshiam2@illinois.edu>
2016-11-12 20:58:21 -0800

    working on search

--- vandyhacks/views.py
+++ vandyhacks/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import auth
-from django.core.context_processors import csrf
+# from django.core.context_processors import csrf
 from django.contrib.auth.forms import UserCreationForm
-from reportlab.pdfgen import canvas
+# from reportlab.pdfgen import canvas
+
+from retrieval import *
+retrieval = Retrieval()
+
 
 
 def graph_view(request, stock_id):
@@ -11,3 +15,11 @@ def graph_view(request, stock_id):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+# def search(query, startdate, enddate):
+# 	date = startdate.split()
+# 	return date
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def search(self, s):
+	return HttpResponse(s)
