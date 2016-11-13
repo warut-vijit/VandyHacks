f306952b714f9a3d0ed61926323cba5ddde0024b
Jay Vaidya <j.vaidya@wustl.edu>
2016-11-13 05:15:50 -0600

    some changes to view.py

--- vandyhacks/views.py
+++ vandyhacks/views.py
@@ -9,28 +9,39 @@ retr = Retrieval()
 
 month_to_number = {"january":1, "february":2, "march":3, "april":4, "may":5 , "june":6, "july":7, "august":8, "september":9, "october":10, "november":11, "december":12}
 
-def default_view(request):
-    #index(request, stockID, one month back, today);
+def default(request):
+    index(request, "MSTR", datetime(2016, 10, 13), datetime(2016, 11, 12));
     return None
 
-def index(request, stock_id, start_date, end_date):
-    stock_data = retrieval.hist_stock(stock_id, start_date, end_date)
-    context = {'sectorList': retrieval.get_industries(), 'stock_data': stock_data}
+#@DUKE to put this in the base.html file, you'll want to use
+
+#{% if sentiment_data in graph_data %}
+
+#to check if the information given contains sentiment data or not.
+#to access stock data you'll need to do {{graph_data.stock_data**}} where  ** is however you access the data in retrieval.py ([i] or .apple or whatever)
+
+def index(request, stock_id, start_date, end_date, sentiment = None):
+    stock_data = retr.hist_stock(stock_id, start_date, end_date)
+    if (sentiment == None):
+        context = {'sectorList': retr.get_industries(), 'graph_data' : {'stock_data': stock_data}}
+    else:
+        context = {'sectorList': retr.get_industries(), 'graph_data' : {'stock_data': stock_data, 'sentiment_data': sentiments}}
     return render(request, 'base.html', context)
 
 
-def search(self, s):
+def search(self, request, s):
 	sentiment_input = s.split(' ')
 
-	if len(sentiment_input) == 6 and sentiment_input[0] == "sentiments":
-		startdate = datetime(int(sentiment_input[3]),int( month_to_number[sentiment_input[2]]),1)
+	if len(sentiment_input) == 7 and sentiment_input[0] == "sentiments":
+		startdate = datetime(int(sentiment_input[4]),int( month_to_number[sentiment_input[3]]),1)
 		print("HELLO" + str(type(startdate)))
-		enddate = datetime(int(sentiment_input[5]),int( month_to_number[sentiment_input[4]]),1)
-		plot = retr.hist_to_date(startdate,enddate,sentiment_input[1])
+		enddate = datetime(int(sentiment_input[6]),int( month_to_number[sentiment_input[5]]),1)
+		sentiment_data = retr.hist_to_date(startdate,enddate,sentiment_input[1])
+        index(request, sentiment_input[2], start_date, end_date, sentiment_data)
 	if len(sentiment_input) == 5:
 		startdate = datetime(int(sentiment_input[2]),int( month_to_number[sentiment_input[1]]),1)
 		enddate = datetime(int(sentiment_input[4]),int( month_to_number[sentiment_input[3]]),1)
-		plot = retr.hist_to_date(startdate,enddate,sentiment_input[1])
+		index(request, sentiment_input[0], startdate, enddate)
 	
-	return HttpResponse(plot)
+	return None
 
